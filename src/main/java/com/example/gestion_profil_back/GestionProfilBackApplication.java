e9d516c0605629008c531090acfc11d932101768
meimoune <maxhmd5050@gmail.com>
2024-09-08 13:17:57 +0000

    Initialisation du projet

---
+++ src/main/java/com/example/gestion_profil_back/GestionProfilBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_profil_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProfilBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProfilBackApplication.class, args);
+	}
+
+}
